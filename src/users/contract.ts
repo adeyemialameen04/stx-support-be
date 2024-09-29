2efe689848903a6697e7fc2f2056149e14d02d8b
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-09-29 05:32:47 +0100

    fix: fixed deployment

--- src/users/contract.ts
+++ src/users/contract.ts
@@ -1,21 +1,36 @@
 import { initContract } from "@ts-rest/core";
 import { z } from "zod";
+import { createPath } from "../utils/path";
 // import { extendZodWithOpenApi } from "@anatine/zod-openapi";
 // extendZodWithOpenApi(z);
 const c = initContract();
 
 export const UserSchema = z.object({
+  uuid: z.string().uuid(),
   username: z.string().optional(),
 });
+export const UserCreateSchema = UserSchema.omit({ uuid: true });
 
 export const contract = c.router({
   getUsers: {
     method: "GET",
-    path: "/users/all",
+    path: createPath("/users/all"),
     responses: {
       200: UserSchema.array(),
     },
     summary: "Get all users",
+    metadata: {
+      openApiTags: ["users"],
+    },
+  },
+  createUser: {
+    method: "POST",
+    path: createPath("/users"),
+    responses: {
+      201: UserSchema,
+    },
+    summary: "create a user",
+    body: UserCreateSchema,
     metadata: {
       openApiTags: ["users"],
     },
