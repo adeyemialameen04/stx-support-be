5a9d5fd3e3359d810eee9641a745f929029e2d0c
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-09-30 02:19:03 +0100

    feat: implementing auth

--- src/api/contracts.ts
+++ src/api/contracts.ts
@@ -1,3 +1,4 @@
+import { authContract } from "../auth/contract";
 import { contract as userContract } from "../users/contract";
 
-export const contracts = [userContract];
+export const contracts = [userContract, authContract];
--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -1,17 +1,6 @@
 import { pgTable, text, uniqueIndex, uuid, varchar } from "drizzle-orm/pg-core";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 
-// username: str | None = Field(default=None, min_length=3, unique=True)
-// name: str | None = Field(None, min_length=3)
-// profile_picture: str | None = Field(None, min_length=3, max_length=100)
-// about_me: str | None = Field(None, min_length=3, max_length=100)
-// #     Stacks stuff
-// prevTxID: str | None = Field(default=None)
-// stx_address_testnet: str | None = Field(default=None, unique=True)
-// btc_address_mainnet: str | None = Field(default=None, unique=True)
-// btc_address_testnet: str | None = Field(default=None, unique=True)
-// pronouns: dict | None = Field(default_factory=dict, sa_column=Column(JSON))
-// bio: str | None = Field(None, min_length=3, max_length=100)
 export const users = pgTable(
   "users",
   {
